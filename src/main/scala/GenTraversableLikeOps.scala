b5112ca2bd541d07ec6fcd828140b7c7df3f28a7
Daniel Nixon <dan.nixon@gmail.com>
2015-09-06 20:33:56 +1000

    Add GenTraversableLikeOps and MapPartial.

---
+++ src/main/scala/GenTraversableLikeOps.scala
@@ -0,0 +1,40 @@
+package org.danielnixon.playwarts
+
+import org.brianmckenna.wartremover.{ WartTraverser, WartUniverse }
+
+object GenTraversableLikeOps extends WartTraverser {
+
+  class Op(name: String, error: String) extends WartTraverser {
+    override lazy val className = "org.danielnixon.playwarts.GenTraversableLikeOps"
+
+    def apply(u: WartUniverse): u.Traverser = {
+      import u.universe._
+
+      val genTraversableLikeSymbol = rootMirror.staticClass("scala.collection.GenTraversableLike")
+      val Name: TermName = name
+      new u.Traverser {
+        override def traverse(tree: Tree): Unit = {
+          tree match {
+            // Ignore trees marked by SuppressWarnings
+            case t if hasWartAnnotation(u)(t) =>
+            case Select(left, Name) if left.tpe.baseType(genTraversableLikeSymbol) != NoType ⇒
+              u.error(tree.pos, error)
+            // TODO: This ignores a lot
+            case LabelDef(_, _, rhs) if isSynthetic(u)(tree) ⇒
+            case _ ⇒
+              super.traverse(tree)
+          }
+        }
+      }
+    }
+  }
+
+  def apply(u: WartUniverse): u.Traverser =
+    WartTraverser.sumList(u)(List(
+      new Op("head", "GenTraversableLike#head is disabled - use GenTraversableLike#headOption instead"),
+      new Op("tail", "GenTraversableLike#tail is disabled - use GenTraversableLike#drop(1) instead"),
+      new Op("init", "GenTraversableLike#init is disabled - use GenTraversableLike#dropRight(1) instead"),
+      new Op("last", "GenTraversableLike#last is disabled - use GenTraversableLike#lastOption instead")
+    ))
+
+}
---
+++ src/main/scala/MapPartial.scala
@@ -0,0 +1,25 @@
+package org.danielnixon.playwarts
+
+import org.brianmckenna.wartremover.{WartTraverser, WartUniverse}
+
+object MapPartial extends WartTraverser {
+  def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+
+    val mapSymbol = rootMirror.staticClass("scala.collection.Map")
+    val ApplyName = TermName("apply")
+    new u.Traverser {
+      override def traverse(tree: Tree) {
+        tree match {
+          // Ignore trees marked by SuppressWarnings
+          case t if hasWartAnnotation(u)(t) =>
+          case Select(left, ApplyName) if left.tpe.baseType(mapSymbol) != NoType =>
+            u.error(tree.pos, "Map#apply is disabled - use Map#get instead")
+          case LabelDef(_, _, rhs) if isSynthetic(u)(tree) =>
+          case _ =>
+            super.traverse(tree)
+        }
+      }
+    }
+  }
+}
