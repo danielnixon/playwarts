91bd1cd9ea04368a2fcb716cd4c521dba73a50dd
Daniel Nixon <dan.nixon@gmail.com>
2015-09-10 21:04:46 +1000

    Add JsReadablePartial.

--- src/main/scala/JsReadablePartial.scala
+++ src/main/scala/JsReadablePartial.scala
@@ -2,19 +2,19 @@ package org.danielnixon.playwarts
 
 import org.brianmckenna.wartremover.{ WartTraverser, WartUniverse }
 
-object JsValuePartial extends WartTraverser {
+object JsReadablePartial extends WartTraverser {
   def apply(u: WartUniverse): u.Traverser = {
     import u.universe._
 
-    val formSymbol = rootMirror.staticClass("play.api.libs.json.JsValue")
-    val GetName = TermName("as")
+    val jsReadableSymbol = rootMirror.staticClass("play.api.libs.json.JsReadable")
+    val AsName = TermName("as")
     new u.Traverser {
       override def traverse(tree: Tree) {
         tree match {
           // Ignore trees marked by SuppressWarnings
           case t if hasWartAnnotation(u)(t) =>
-          case Select(left, GetName) if left.tpe.baseType(formSymbol) != NoType =>
-            u.error(tree.pos, "JsValue#as is disabled - use JsValue#asOpt instead")
+          case Select(left, AsName) if left.tpe.baseType(jsReadableSymbol) != NoType =>
+            u.error(tree.pos, "JsReadable#as is disabled - use JsReadable#asOpt instead")
           case LabelDef(_, _, rhs) if isSynthetic(u)(tree) =>
           case _ =>
             super.traverse(tree)
