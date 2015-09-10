ba4085ec131009b3915074e5be5e5c7a9871a111
Daniel Nixon <dan.nixon@gmail.com>
2015-09-10 22:01:04 +1000

    Add GlobalSettings.

---
+++ src/main/scala/GlobalSettings.scala
@@ -0,0 +1,23 @@
+package org.danielnixon.playwarts
+
+import org.brianmckenna.wartremover.{WartTraverser, WartUniverse}
+
+object GlobalSettings extends WartTraverser {
+  def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+
+    val globalSymbol = rootMirror.staticClass("play.api.GlobalSettings")
+
+    new u.Traverser {
+      override def traverse(tree: Tree): Unit = {
+        tree match {
+          // Ignore trees marked by SuppressWarnings
+          case t if hasWartAnnotation(u)(t) =>
+          case t: ImplDef if t.symbol.typeSignature.baseClasses.contains(globalSymbol) =>
+            u.error(tree.pos, "play.api.GlobalSettings is disabled - use dependency injection instead")
+          case t => super.traverse(tree)
+        }
+      }
+    }
+  }
+}
