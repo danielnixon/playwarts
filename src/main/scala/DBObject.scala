f3bfe5bead4e417c87a653f8c7f95af0190bcaac
Daniel Nixon <dan.nixon@gmail.com>
2015-09-10 21:21:13 +1000

    Add DBObject.

---
+++ src/main/scala/DBObject.scala
@@ -0,0 +1,25 @@
+package org.danielnixon.playwarts
+
+import org.brianmckenna.wartremover.{WartTraverser, WartUniverse}
+
+object DBObject extends WartTraverser {
+  def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+
+    val cacheObject = rootMirror.staticModule("play.api.db.DB")
+
+    new u.Traverser {
+      override def traverse(tree: Tree): Unit = {
+        tree match {
+          // Ignore trees marked by SuppressWarnings
+          case t if hasWartAnnotation(u)(t) =>
+          case Select(tpt, _) if tpt.tpe.contains(cacheObject) => {
+            u.error(tree.pos, "play.api.db.DB is disabled - use DBApi or Database instead")
+            super.traverse(tree)
+          }
+          case _ => super.traverse(tree)
+        }
+      }
+    }
+  }
+}
