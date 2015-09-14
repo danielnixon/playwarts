fdcf047ccd1643ccba5eb2dc2ec41324342ae316
Daniel Nixon <dan.nixon@gmail.com>
2015-09-15 09:44:37 +1000

    Tests for FormPartial.

---
+++ src/test/scala/FormPartialTest.scala
@@ -0,0 +1,33 @@
+import org.brianmckenna.wartremover.test.WartTestTraverser
+import org.danielnixon.playwarts.FormPartial
+import org.scalatest.FunSuite
+import play.api.data.Form
+import play.api.data.Forms.text
+
+class FormPartialTest extends FunSuite {
+  test("can't use Form#get") {
+    val result = WartTestTraverser(FormPartial) {
+      Form("foo" -> text).get
+    }
+    assertResult(List("Form#get is disabled - use Form#fold instead"), "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("doesn't detect other `get` methods") {
+    val result = WartTestTraverser(FormPartial) {
+      case class A(get: Int)
+      println(A(1).get)
+    }
+    assertResult(List.empty, "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("FormPartial wart obeys SuppressWarnings") {
+    val result = WartTestTraverser(FormPartial) {
+      @SuppressWarnings(Array("org.danielnixon.playwarts.FormPartial"))
+      val foo = Form("foo" -> text).get
+    }
+    assertResult(List.empty, "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+}
