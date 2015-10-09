1ee7312b281dd4b3f05aa1c14e652c546d0fab99
Daniel Nixon <dan.nixon@gmail.com>
2015-10-09 19:18:58 +1100

    Disable TraversableOnce's min, max, minBy and maxBy methods.

--- src/main/scala/TraversableOnceOps.scala
+++ src/main/scala/TraversableOnceOps.scala
@@ -4,6 +4,10 @@ object TraversableOnceOps extends ClassMultiWart(
   "org.danielnixon.playwarts.TraversableOnceOps",
   "scala.collection.TraversableOnce",
   List(
-    "reduceLeft" -> "TraversableOnce#reduceLeft is disabled - use TraversableOnce#reduceLeftOption or TraversableOnce#foldLeft instead"
+    "reduceLeft" -> "TraversableOnce#reduceLeft is disabled - use TraversableOnce#reduceLeftOption or TraversableOnce#foldLeft instead",
+    "max" -> "TraversableOnce#max is disabled - use TraversableOnce#reduceLeftOption instead",
+    "min" -> "TraversableOnce#min is disabled - use TraversableOnce#reduceLeftOption instead",
+    "maxBy" -> "TraversableOnce#maxBy is disabled - use TraversableOnce#reduceLeftOption instead",
+    "minBy" -> "TraversableOnce#minBy is disabled - use TraversableOnce#reduceLeftOption instead"
   )
 )
--- src/test/scala/TraversableOnceOpsTest.scala
+++ src/test/scala/TraversableOnceOpsTest.scala
@@ -16,6 +16,38 @@ class TraversableOnceOpsTest extends FunSuite {
     assertResult(List.empty, "result.warnings")(result.warnings)
   }
 
+  test("can't use TraversableOnce#max") {
+    val result = WartTestTraverser(TraversableOnceOps) {
+      println(list.max)
+    }
+    assertResult(List("TraversableOnce#max is disabled - use TraversableOnce#reduceLeftOption instead"), "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("can't use TraversableOnce#min") {
+    val result = WartTestTraverser(TraversableOnceOps) {
+      println(list.min)
+    }
+    assertResult(List("TraversableOnce#min is disabled - use TraversableOnce#reduceLeftOption instead"), "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("can't use TraversableOnce#maxBy") {
+    val result = WartTestTraverser(TraversableOnceOps) {
+      println(list.maxBy(x => x))
+    }
+    assertResult(List("TraversableOnce#maxBy is disabled - use TraversableOnce#reduceLeftOption instead"), "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("can't use TraversableOnce#minBy") {
+    val result = WartTestTraverser(TraversableOnceOps) {
+      println(list.minBy(x => x))
+    }
+    assertResult(List("TraversableOnce#minBy is disabled - use TraversableOnce#reduceLeftOption instead"), "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+
   test("TraversableOnceOps wart obeys SuppressWarnings") {
     val result = WartTestTraverser(TraversableOnceOps) {
       @SuppressWarnings(Array("org.danielnixon.playwarts.TraversableOnceOps"))
