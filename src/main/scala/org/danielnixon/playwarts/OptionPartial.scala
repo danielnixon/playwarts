aa1deda4d75a9d3ba761f21524ccc4409fa95236
Daniel Nixon <dan.nixon@gmail.com>
2016-01-09 12:30:01 +1100

    Add OptionPartial wart.

---
+++ src/main/scala/org/danielnixon/playwarts/OptionPartial.scala
@@ -0,0 +1,6 @@
+package org.danielnixon.playwarts
+
+object OptionPartial extends ClassWart(
+  "scala.Option",
+  "orNull",
+  "Option#orNull is disabled")
---
+++ src/test/scala/OptionPartialTest.scala
@@ -0,0 +1,23 @@
+import org.brianmckenna.wartremover.test.WartTestTraverser
+import org.danielnixon.playwarts.OptionPartial
+import org.scalatest.FunSuite
+
+class OptionPartialTest extends FunSuite {
+
+  test("can't use Option#orNull") {
+    val result = WartTestTraverser(OptionPartial) {
+      val foo = None.orNull
+    }
+    assertResult(List("Option#orNull is disabled"), "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("OptionPartial wart obeys SuppressWarnings") {
+    val result = WartTestTraverser(OptionPartial) {
+      @SuppressWarnings(Array("org.danielnixon.playwarts.OptionPartial"))
+      val foo = None.orNull
+    }
+    assertResult(List.empty, "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+}
