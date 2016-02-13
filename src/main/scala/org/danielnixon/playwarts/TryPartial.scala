92431c594a39de9d6776bc0601c5acc70708f9c7
Daniel Nixon <dan.nixon@gmail.com>
2016-02-13 22:13:46 +1100

    Remove TryPartial wart.

--- src/main/scala/org/danielnixon/playwarts/TryPartial.scala
+++
@@ -1,7 +0,0 @@
-package org.danielnixon.playwarts
-
-object TryPartial extends ClassMultiWart(
-  "org.danielnixon.playwarts.TryPartial",
-  "scala.util.Try",
-  List("get" -> "Try#get is disabled - use Try#getOrElse instead")
-)
