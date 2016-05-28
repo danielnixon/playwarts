14b08e485c2d7f774091052cdf57abe6d57f5623
Daniel Nixon <dan.nixon@gmail.com>
2016-05-28 18:02:46 +1000

    Compile-time only dependency on playwarts.

--- sbt-plugin/src/main/scala/org/danielnixon/playwarts/PlayWarts.scala
+++ sbt-plugin/src/main/scala/org/danielnixon/playwarts/PlayWarts.scala
@@ -10,6 +10,9 @@ object PlayWarts extends AutoPlugin {
   private val home = Path.userHome.getAbsolutePath
   private val version = buildinfo.BuildInfo.version
 
+  // http://stackoverflow.com/a/21516954/2476884
+  private val compileOnly = "compileonly"
+
   object autoImport {
     val PlayWart = org.danielnixon.playwarts.PlayWart
   }
@@ -19,7 +22,9 @@ object PlayWarts extends AutoPlugin {
   override def requires = WartRemover
 
   override lazy val projectSettings = Seq(
-    libraryDependencies += "org.danielnixon" % "playwarts_2.11" % version,
+    ivyConfigurations += config(compileOnly).hide,
+    libraryDependencies += "org.danielnixon" % "playwarts_2.11" % version % compileOnly,
+    unmanagedClasspath in Compile ++= update.value.select(configurationFilter(compileOnly)),
     wartremoverClasspaths ++= Seq(
       s"file:$home/.ivy2/local/org.danielnixon/playwarts_2.11/$version/jars/playwarts_2.11.jar",
       s"file:$home/.ivy2/cache/org.danielnixon/playwarts_2.11/jars/playwarts_2.11-$version.jar"
