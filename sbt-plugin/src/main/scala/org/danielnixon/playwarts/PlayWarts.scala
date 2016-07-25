d16b9086ca916012aeadd465a6d294581133b423
Daniel Nixon <dan.nixon@gmail.com>
2016-07-25 23:18:49 +1000

    Don't hardcode path to jar. Fixes #24.

--- sbt-plugin/src/main/scala/org/danielnixon/playwarts/PlayWarts.scala
+++ sbt-plugin/src/main/scala/org/danielnixon/playwarts/PlayWarts.scala
@@ -7,7 +7,6 @@ import wartremover.WartRemover.autoImport.{ wartremoverClasspaths, wartremoverEx
 
 object PlayWarts extends AutoPlugin {
 
-  private val home = Path.userHome.getAbsolutePath
   private val version = buildinfo.BuildInfo.version
   private val artifactID = buildinfo.BuildInfo.artifactID
   private val organization = buildinfo.BuildInfo.organization
@@ -22,10 +21,12 @@ object PlayWarts extends AutoPlugin {
 
   override lazy val projectSettings = Seq(
     libraryDependencies += organization % artifactID % version % Provided,
-    wartremoverClasspaths ++= Seq(
-      s"file:$home/.ivy2/local/$organization/$artifactID/$version/jars/$artifactID.jar",
-      s"file:$home/.ivy2/cache/$organization/$artifactID/jars/$artifactID-$version.jar"
-    ),
+    wartremoverClasspaths ++= {
+      (dependencyClasspath in Compile).value.files
+        .find(_.name.contains(artifactID))
+        .map(j => s"file:${j.absolutePath}")
+        .toSeq
+    },
     wartremoverExcluded ++= Seq(
       crossTarget.value / "routes" / "main" / "controllers" / "ReverseRoutes.scala",
       crossTarget.value / "routes" / "main" / "controllers" / "javascript" / "JavaScriptReverseRoutes.scala",
